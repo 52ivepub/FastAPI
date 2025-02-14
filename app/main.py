00d999534d97ca2a71b31145b5c68c664654cba3
Yugenyi Busygin <abusyginza@gmail.com>
2025-02-14 07:56:02 +0700

    validation

--- app/main.py
+++ app/main.py
@@ -1,14 +1,45 @@
 from fastapi import FastAPI, Query
 from typing import Optional
 from datetime import date
+from pydantic import BaseModel 
 
 app = FastAPI()
 
-@app.get("/hotel/{hotel_id}")
+class SHotel(BaseModel):
+    adress: str
+    name: str
+    stars: int
+
+
+
+
+
+@app.get("/hotels")
 def foo(location: str,
         date_from: date,
         date_to: date,
         has_spa: Optional[bool]=None,
-        stars: Optional[int]=Query(None, ge=1, le=5)):
-    return hotel_id, date_from, date_to
+        stars: Optional[int]=Query(None, ge=1, le=5)) -> list[SHotel]:
+
+
+    hotels = [
+        {
+            "adress": 'ул. Гагарина, 1, Алтай',
+            "name": 'Super Hotel',
+            "stars": 5,
+        },
+    ]
+    return hotels
+ 
+
+class SBoking(BaseModel):
+    room_id : int
+    date_from: date
+    date_to: date
+
+@app.post('/bookings')
+def add_boking(SBoking):
+    pass
+
+
 
