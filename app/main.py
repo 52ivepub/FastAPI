0ce80a93f79290a1e5202bf2c6e3c71b79a39d77
Yugenyi Busygin <abusyginza@gmail.com>
2025-02-15 18:56:22 +0700

    ORM

--- app/main.py
+++ app/main.py
@@ -5,6 +5,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class HotelsSearchArghs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool]=None,
+        stars: Optional[int]=Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
 class SHotel(BaseModel):
     adress: str
     name: str
