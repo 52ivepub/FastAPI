7d2abf44a71338302986d0311ecee551ec9bb640
PC_evgenyi <abusyginza@gmail.com>
2025-02-20 08:22:08 +0700

    migrate in postgres

--- app/datebase.py
+++ app/datebase.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asynio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
---
+++ app/hotels/models.py
@@ -0,0 +1,16 @@
+from ctypes.wintypes import SERVICE_STATUS_HANDLE
+from os import name
+
+from sqlalchemy import JSON, Column, Integer, String
+from app.datebase import Base
+
+
+class Hotels(Base):
+    __tablename__ = "hotels"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    services = Column(JSON)
+    rooms_quantity = Column(Integer, nullable=False)
+    image_id = Column(Integer)
