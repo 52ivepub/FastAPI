9dbaf9a0682f7b8ead2666f49a66d07d474c2bce
PC_evgenyi <abusyginza@gmail.com>
2025-02-21 20:50:05 +0700

    load data

--- app/rooms/models.py
+++ app/rooms/models.py
@@ -1,3 +1,4 @@
+from annotated_types import T
 from sqlalchemy import JSON, Column, Integer, String
 from app.datebase import Base
 
@@ -6,13 +7,13 @@ class Rooms(Base):
     __tablename__ = "rooms"
 
     id = Column(Integer, primary_key=True)
-    hotel_id = Column(Integer)
+    hotel_id = Column(Integer, nullable=False)
     name = Column(String, nullable=False)
-    description = Column(String, nullable=False)
+    description = Column(String, nullable=True)
     price = Column(Integer, nullable=False)
-    services = Column(JSON)
+    services = Column(JSON, nullable=True)
     quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer, nullable=False)
+    image_id = Column(Integer, nullable=True)
     
     
     
