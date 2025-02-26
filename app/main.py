21ec3368259b8371c64c45a3b38c33fe6ee954bf
PC_evgenyi <abusyginza@gmail.com>
2025-02-26 20:47:54 +0700

    autenfication_valid

--- app/main.py
+++ app/main.py
@@ -2,12 +2,15 @@ from fastapi import FastAPI, Query
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel 
+from app.users.router import router as router_users
 from app.bookings.router import router as router_bookings
+
 
 
 app = FastAPI()
 
-app.include_router(router_bookings )
+app.include_router(router_bookings)
+app.include_router(router_users)
 
 class HotelsSearchArghs:
     def __init__(
